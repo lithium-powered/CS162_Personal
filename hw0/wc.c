f074bdd0747135d037204aa9ffa1a3aae2584d7d
Liquin Yu <liyu@berkeley.edu>
2016-01-28 23:21:42 +0000

    wrote wc.c.

--- hw0/wc.c
+++ hw0/wc.c
@@ -1,3 +1,39 @@
+#include <stdio.h>
+
 int main(int argc, char *argv[]) {
-    return 0;
+	FILE *file;
+	char buf[1];
+
+	int charNum = 0;
+	int wordNum = 0;
+	int lineNum = 0;
+	char prev;
+	char cur;
+
+	if (argc > 1){
+		file = fopen(argv[1], "r");
+		while (!feof(file)){
+			fread(buf, 1, 1, file);
+			if (feof(file)){
+				if (!isspace(prev)){
+					wordNum += 1;
+				}
+				break;
+			}
+			charNum += 1;
+			cur = buf[0];
+			if (isspace(cur)){
+				if ((charNum != 0) && (!isspace(prev))){
+					wordNum += 1;
+				}
+				if (cur == '\n'){
+					lineNum += 1;
+				}
+			}
+			prev = cur;
+		}
+	}
+	printf("%d %d %d %s\n", lineNum, wordNum, charNum, argv[1]);
+
+    return 1;
 }
