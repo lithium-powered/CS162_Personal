a4cd937989fe33f5b0e56af17f980d99e48d5981
Liquin Yu <liyu@berkeley.edu>
2016-01-29 07:02:08 +0000

    Updated wc.c.

--- hw0/wc.c
+++ hw0/wc.c
@@ -10,30 +10,37 @@ int main(int argc, char *argv[]) {
 	char prev;
 	char cur;
 
-	if (argc > 1){
+	if (argc >1){
 		file = fopen(argv[1], "r");
-		while (!feof(file)){
-			fread(buf, 1, 1, file);
-			if (feof(file)){
-				if (!isspace(prev)){
-					wordNum += 1;
-				}
-				break;
-			}
-			charNum += 1;
-			cur = buf[0];
-			if (isspace(cur)){
-				if ((charNum != 0) && (!isspace(prev))){
-					wordNum += 1;
-				}
-				if (cur == '\n'){
-					lineNum += 1;
-				}
-			}
-			prev = cur;
-		}
+	}else{
+		file = stdin;
 	}
-	printf("%d %d %d %s\n", lineNum, wordNum, charNum, argv[1]);
+	while (!feof(file)){
+		fread(buf, 1, 1, file);
+		if (feof(file)){
+			if (!isspace(prev)){
+				wordNum += 1;
+			}
+			break;
+		}
+		charNum += 1;
+		cur = buf[0];
+		if (isspace(cur)){
+			if ((charNum != 0) && (!isspace(prev))){
+				wordNum += 1;
+			}
+			if (cur == '\n'){
+				lineNum += 1;
+			}
+		}
+		prev = cur;
+	}
+
+	if (argc >1){
+		printf("%d %d %d %s\n", lineNum, wordNum, charNum, argv[1]);
+	}else{
+		printf("%d %d %d\n", lineNum, wordNum, charNum);
+	}
 
     return 1;
 }
