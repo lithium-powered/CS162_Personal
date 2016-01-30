c6c2cb8052eef57494d703baf3c031947502f676
Liquin Yu <liyu@berkeley.edu>
2016-01-30 00:35:33 +0000

    added main.c.

--- hw0/main.c
+++ hw0/main.c
@@ -3,8 +3,8 @@
 
 int main() {
     struct rlimit lim;
-    printf("stack size: %ld\n", 0L);
-    printf("process limit: %ld\n", 0L);
-    printf("max file descriptors: %ld\n", 0L);
+    printf("stack size: %ld\n", RLIMIT_STACK);
+    printf("process limit: %ld\n", RLIMIT_CPU);
+    printf("max file descriptors: %ld\n", RLIMIT_NOFILE-1);
     return 0;
 }
