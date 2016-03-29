567566c6fb3b57d564658f916274b926429a87d8
Liquin Yu <liyu@berkeley.edu>
2016-03-29 11:24:57 +0000

    Added memory freeing.

--- hw3/mm_alloc.c
+++ hw3/mm_alloc.c
@@ -52,9 +52,11 @@ void *mm_malloc(size_t size) {
             zeroData(currentMeta);
             return currentMeta + headerSize;
         }
-        currentMeta->free = 0;
-        zeroData(currentMeta);
-        return currentMeta + headerSize;
+        if (currentMeta->free){
+            currentMeta->free = 0;
+            zeroData(currentMeta);
+            return currentMeta + headerSize;
+        }
         currentMeta = currentMeta->next;
     }
     /*if ((currentMeta = sbrk(size+headerSize))){
