e7a16b86d93840a92f2bd23ca972ac9bbd486cfc
Liquin Yu <liyu@berkeley.edu>
2016-03-30 00:55:13 +0000

    realloc.

--- hw3/mm_alloc.c
+++ hw3/mm_alloc.c
@@ -88,7 +88,12 @@ void *mm_realloc(void *ptr, size_t size) {
         return NULL;
     }
     struct block *currentMeta = (struct block*)(((char *) ptr) - headerSize);
-    memcpy((char *)newBlock, (char *) ptr, currentMeta->size);
+    if (currentMeta->size <= size){
+        memcpy((char *)newBlock, (char *) ptr, currentMeta->size);
+        memset((char *)newBlock+currentMeta->size,0,size - currentMeta->size);
+    }else{
+        memcpy((char *)newBlock, (char *) ptr, size);
+    }
     mm_free(ptr);
 
     return newBlock;
