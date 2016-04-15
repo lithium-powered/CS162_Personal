03565f72490a220fa7a880b46093fd65295de7ef
Liquin Yu <liyu@berkeley.edu>
2016-04-15 23:29:50 +0000

    Leader returning no key

--- hw4/tpcleader.c
+++ hw4/tpcleader.c
@@ -206,6 +206,7 @@ void tpcleader_handle_tpc(tpcleader_t *leader, kvrequest_t *req, kvresponse_t *r
   for(counter = 0; counter < leader->redundancy; counter++){
     sockfd = elem->sockfd;
     kvrequest_send(req, sockfd);
+    //kvresponse_receive(res, sockfd);
     /*
     while(!(res->type == ACK)){
       kvresponse_receive(res, sockfd);
@@ -218,6 +219,10 @@ void tpcleader_handle_tpc(tpcleader_t *leader, kvrequest_t *req, kvresponse_t *r
     res->type = SUCCESS;
   }else{
     res->type = ERROR;
+    strcpy(res->body, ERRMSG_GENERIC_ERROR);
+  }
+  kvresponse_receive(res, head->sockfd);
+  if (res->type != ACK){
     strcpy(res->body, ERRMSG_GENERIC_ERROR);
   }
 
