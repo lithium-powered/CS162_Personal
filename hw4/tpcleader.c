d8385965517a52f0301879bba5398d63dc2685f2
Liquin Yu <liyu@berkeley.edu>
2016-04-15 00:06:56 +0000

    Task 1.1

--- hw4/tpcleader.c
+++ hw4/tpcleader.c
@@ -207,8 +207,8 @@ void tpcleader_handle_tpc(tpcleader_t *leader, kvrequest_t *req, kvresponse_t *r
     sockfd = elem->sockfd;
     kvrequest_send(req, sockfd);
     while(!kvresponse_receive(res, sockfd)){
+      kvrequest_send(req, sockfd);
     }
-    kvrequest_send(req, sockfd);
     elem = elem->next;
   }
   if(commit){
