84a23c757e608f07f5e15a8e86efe9b329907aed
Liquin Yu <liyu@berkeley.edu>
2016-04-16 06:30:57 +0000

    Leader returning no key

--- hw4/tpcfollower.c
+++ hw4/tpcfollower.c
@@ -173,6 +173,7 @@ void tpcfollower_handle_tpc(tpcfollower_t *server, kvrequest_t *req, kvresponse_
     }
   }else if(req->type == ABORT){
     res->type = ACK;
+    server->state = TPC_READY;
   }else{
     res->type = ERROR;
     strcpy(res->body, ERRMSG_GENERIC_ERROR);
